4b1362ea828227bf421f3ee2f53aa19c379b362c
mscoggs <matthewscoggins13@gmail.com>
2024-02-28 08:27:03 -0500

    .

---
+++ trees_for_m/submit_jobs.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+
+sbatch skx0
+
+sbatch skx1
+
+sbatch skx2
+
+sbatch skx3
+
+sbatch skx4
+
+sbatch skx5
+
+sbatch skx6
+
+sbatch skx7
+
+sbatch skx8
+
+sbatch skx9
+
+sbatch skx10
+
+sbatch skx11
+
+sbatch skx12
+
+sbatch skx13
+
+sbatch skx14
+
+sbatch skx15
+
+sbatch skx16
+
+sbatch skx17
+
+sbatch skx18
+
+sbatch skx19
+
